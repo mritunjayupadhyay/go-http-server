af5e2bec976c2915a0e679600fd6228dd1fe20ff
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2022-12-19 18:28:56 +0700

    feat: Create hello world get api

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
+
+func handleHelloFunc(w http.ResponseWriter, r *http.Request) {
+	// Check if path is correct
+	if r.URL.Path != "/hello" {
+		http.Error(w, "path not found", 404)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Incorrect Method", 401)
+		return
+	}
+	fmt.Fprintf(w, "hello from hello handler 2")
+}
 
 func main() {
 	fmt.Printf("Hello world")
+	http.HandleFunc("/hello", handleHelloFunc)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
